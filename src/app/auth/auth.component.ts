ee7fb9c5d7aa529608006796f168da16963361c9
AEE <myfutureiscode@gmail.com>
2022-04-30 02:03:46 +0200

    feat: create basic look of login screen

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthComponent {}
