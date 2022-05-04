58e702db963c2a042312ebf796e0e83b97035907
krzt96 <krzysztoftokarski96@gmail>
2022-05-04 20:46:56 +0200

    feat: add very basic favlist feature

--- src/app/features/ordering/ordering-panel/favourite-ice-cream-list/favourite-ice-cream-list.component.ts
+++ src/app/features/ordering/ordering-panel/favourite-ice-cream-list/favourite-ice-cream-list.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Icecream } from '@shared/models/ice-cream/icecream.interface';
+import { AppState } from '@state/app.state';
 
+import { Observable, take } from 'rxjs';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
 @Component({
   selector: 'icy-favourite-ice-cream-list',
   templateUrl: './favourite-ice-cream-list.component.html',
   styleUrls: ['./favourite-ice-cream-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FavouriteIceCreamListComponent implements OnInit {
+export class FavouriteIceCreamListComponent {
+  public icecreamList$!: Observable<Icecream[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<AppState>, private firestore: Firestore) {
+    const selectUid = (state: AppState) => state.user.currentUser?.uid;
+    // to do
+    this.store
+      .select(selectUid)
+      .pipe(take(1))
+      .subscribe(uid => {
+        const collectionRef = collection(this.firestore, `users/${uid}/favIcecreamList`);
+        this.icecreamList$ = collectionData(collectionRef) as Observable<Icecream[]>;
+      });
   }
 
+  public onClick(icecream: Icecream) {}
 }
